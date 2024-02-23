59d48ebb7d784b754c638ce42f13f8ecc8153413
Hugopinchemel <hugo.pinchemel@mmibordeaux.com>
2024-02-23 10:28:41 +0100

    Add background image and form control styles, implement glitter effect, and display zodiac signs

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+function glitter() {
+    const stars = document.querySelectorAll('.star');
+  
+    stars.forEach(star => {
+      const size = Math.random() * 5;
+      star.style.width = `${size}px`;
+      star.style.height = `${size}px`;
+      star.style.left = `${Math.random() * innerWidth}px`;
+      star.style.top = `${Math.random() * innerHeight}px`;
+    });
+  }
+  
+setInterval(glitter, 1000);
+alert('ok');
